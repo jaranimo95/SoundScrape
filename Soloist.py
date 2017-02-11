21f65a283b2a4cb0b4a66616cd21ed421c28e925
pretentiousmusician <brendanjkerr@gmail.com>
2017-02-11 11:51:43 -0500

    Basic output working

--- Soloist.py
+++ Soloist.py
@@ -10,36 +10,53 @@ class Soloist:
 
     song = []
 
+    phrases = [[0, 2, 1, 0], [0, 1, 0], [4, 3, 2, 1], [4, 5, 0, 4], [0], [3, 4, 5, 5], [4, 5, 4, 5, 4, 5, 4, 5, 4, 5]]
+
     h_step = math.pow(2, (1/12))
 
-    def __init__(self, notes, root):
+    def __init__(self, chars, root):
+        seg = math.floor(len(chars)/48)
+        input = chars[:-(len(chars) % 48)]
+        notes = []
+        hash = 0
+        for ind in range(48):
+            for ch in input[ind*seg:len(input) - ind*seg]:
+                hash = (hash + ord(ch)) % 6
+            notes.append(hash)
         first = self.get_scale(self.chrom[root])
         fourth = self.get_scale(self.chrom[(root + 5) % 12])
         fifth = self.get_scale(self.chrom[(root + 7) % 12])
         #Key of A, start on root
         self.song.append(self.get_phrase(first, root))
-        for i in range(len(notes)):
+        for i in range(len(notes)-1):
             bar = i % 12
             #Assuming that notes is a string of chars 0-6
-            if bar < 4 or (bar > 5 & bar < 8) or bar == 10:
+            if bar < 4 or (bar > 5 and bar < 8) or bar == 10:
                 self.song.append(self.get_phrase(first, notes[i]))
             elif bar < 6 or bar == 9:
                 self.song.append(self.get_phrase(fourth, notes[i]))
             else:
                 self.song.append(self.get_phrase(fifth, notes[i]))
+            self.song.append(self.get_phrase(fifth, 0))
 
     def get_scale(self, root):
         scale = []
         for steps in self.ratios:
             scale.append(root*math.pow(self.h_step, steps))
+        print()
         return scale
 
     def get_song(self):
         return self.song
 
     def get_phrase(self, mode, inval):
-        seq = random.choice([[0, 2, 1, 0], [0, 1, 0], [4, 3, 2, 1], [4, 5, 0, 4], [0]])
+        seq = random.choice(self.phrases)
         phrase = []
         for item in seq:
-            phrase.append(mode[(item+inval) % len(mode)])
+            if item is None:
+                phrase.append(0)
+            elif item >= len(mode):
+                phrase.append(mode[(item+inval) % len(mode)]*2)
+            else:
+                phrase.append(mode[(item+inval) % len(mode)])
         return tuple(phrase)
--- output.py
+++ output.py
@@ -2,17 +2,18 @@ import pyaudio
 import numpy as np
 
 
-def playaudio(*freqs):
+def playaudio(freqs):
 
-    for i in freqs:
+    for i in freqs:                 #freqs is a list of phrases(tuples)
         p = pyaudio.PyAudio()
         volume = 0.5                # range [0.0, 1.0]
         fs = 44100                  # sampling rate, Hz, must be integer
-        duration = 0.5/len(freqs)   # in seconds, may be float
-        f = i                       # sine frequency, Hz, may be float
+        duration = 2/len(i)   # in seconds, may be float
+        #f = i                       # sine frequency, Hz, may be float
 
         # generate samples, note conversion to float32 array
-        samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
+        for f in i:
+            samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
         # for paFloat32 sample values must be in range [-1.0, 1.0]
         stream = p.open(format=pyaudio.paFloat32,
                         channels=1,
