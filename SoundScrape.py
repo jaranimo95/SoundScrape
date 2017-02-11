f76b2da987bb509b9adb8b1d13e061c3037de421
jaranimo95 <cjarani1608@hotmail.com>
2017-02-11 15:27:07 -0500

    output function works, included it in our main file (SoundScrape.py) so you guys can call it w your functions. also you can test it w te output.py file I built it in.

---
+++ SoundScrape.py
@@ -0,0 +1,31 @@
+import pyaudio
+import numpy as np
+
+
+def playaudio(freqs):
+
+    freqs = {200, 800, 300, 600}
+
+    p = pyaudio.PyAudio()
+    for f in freqs:                 # for each sine frequency, in Hz (float)
+        volume = 0.5                # amplitude [0.0, 1.0]
+        fs = 44100                  # sampling rate, Hz, must be integer
+        duration = 2.0/len(freqs)   # in seconds, may be float
+
+        # generate samples, note conversion to float32 array
+        samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
+        # for paFloat32 sample values must be in range [-1.0, 1.0]
+        stream = p.open(format=pyaudio.paFloat32,
+                        channels=1,
+                        rate=fs,
+                        output=True)
+
+        # play. May repeat with different volume values (if done interactively)
+        stream.write(volume*samples)
+
+        # testshit()
+
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
